523e9e3d4174aa6c4b3dc98942a12f1e3cc1a37f
Roshan Pokhrel <60165267+Roshan-ops@users.noreply.github.com>
2024-08-09 18:36:57 +0545

    Calender

--- cypress/Pages/registerPage.cy.js
+++ cypress/Pages/registerPage.cy.js
@@ -6,7 +6,12 @@ export class registerPage{
     firstname:'#signupStaffDetailForm-firstName',
     Middlename:'#signupStaffDetailForm-middleName',
     Lastname:'#signupStaffDetailForm-lastName',
-    Dob:'#signupStaffDetailForm-dateOfBirth'
+    Dob:'#signupStaffDetailForm-dateOfBirth',
+    year:'.mdc-button__label > span',
+    chooseMonthYearBtn: 'button[aria-label="Choose month and year"]',
+    calendarContent: '.mat-calendar-content',
+    calendarBody: '.mat-calendar-body',
+    overlayBackdrop: '.cdk-overlay-backdrop'
     }
     openURL(){
         cy.visit(Cypress.env('URL'))
@@ -28,5 +33,35 @@ enterlastname(LName){
 clickdob(){
     cy.get(this.weblocators.Dob).click()
 }
+clickyear(){
+    cy.get(this.weblocators.year).click()
+}
 
+selectRandomYear() {
+    cy.get(this.weblocators.chooseMonthYearBtn).click();
+    cy.get(this.weblocators.calendarContent).within(() => {
+        cy.get('.mat-calendar-body-cell-content').then(years => {
+            const randomYearIndex = Cypress._.random(0, years.length - 1);
+            cy.wrap(years[randomYearIndex]).click();
+        });
+    });
+}
+
+selectRandomMonth() {
+    cy.get(this.weblocators.calendarContent).within(() => {
+        cy.get('.mat-calendar-body-cell-content').then(months => {
+            const randomMonthIndex = Cypress._.random(0, months.length - 1);
+            cy.wrap(months[randomMonthIndex]).click();
+        });
+    });
+}
+
+selectRandomDay() {
+    cy.get(this.weblocators.calendarBody).within(() => {
+        cy.get('.mat-calendar-body-cell-content').then(days => {
+            const randomDayIndex = Cypress._.random(0, days.length - 1);
+            cy.wrap(days[randomDayIndex]).click();
+        });
+    });
+} 
 }
